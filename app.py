fc261bd94eded47156133667124d39b568d097b1
Sorin <sorinnechiforora@gmail.com>
2023-01-28 12:52:31 +0000

    Add better error handling for / route

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import abort, Flask, render_template
+import werkzeug
 
 app = Flask(__name__)
 
-@app.route("/")
+app.register_error_handler(404, werkzeug.exceptions.NotFound)
+
+@app.route("/", methods = ["GET"])
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
