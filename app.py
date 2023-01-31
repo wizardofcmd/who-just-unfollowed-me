666171a372f3f315628675ead4fea778107b3fa8
Sorin <sorinnechiforora@gmail.com>
2023-01-31 19:24:25 +0000

    Add tweepy OAuth2 User Handling to /login

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, redirect, request, url_for
+import tweepy
 import werkzeug
 
 app = Flask(__name__)
@@ -9,5 +11,23 @@ app.register_error_handler(404, werkzeug.exceptions.NotFound)
 
 @app.route("/", methods=["GET"])
 def index():
-    app.logger.info(f"{app.config}")
     return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    redirect_uri = url_for("authorize", _external=True)
+    client_id = app.config.get("TWITTER_CLIENT_ID")
+    client_secret = app.config.get("TWITTER_CLIENT_SECRET")
+    
+    auth = tweepy.OAuth2UserHandler(
+        redirect_uri=redirect_uri, client_id=client_id, 
+        client_secret=client_secret, 
+        scope=["tweet.read users.read follows.read"]
+    )
+    return redirect(auth.get_authorization_url())
+
+
+@app.route("/authorize", methods=["GET", "POST"])
+def authorize():
+    return "Authorized?"
